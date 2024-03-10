777dd074dad50e6dc134d6aa9df128ab29801ad9
Oleksandr Molodchyk <molodchykr@gmail.com>
2024-03-10 07:36:29 +0100

    GPL license

--- background.js
+++ background.js
@@ -1,3 +1,27 @@
+/*
+ * YouTube Mix Blocker Extension
+ *
+ * file: background.js
+ * 
+ * This file is part of the YouTube Mix Blocker Extension.
+ *
+ * Defense YouTube Mix Blocker Extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * YouTube Mix Blocker Extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Defense Against Distractions Extension. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Oleksandr Molodchyk
+ * Copyright (C) 2023-2024 Oleksandr Molodchyk
+ */
+
 chrome.runtime.onInstalled.addListener(() => {
   // Set up rules to keep the service worker active for YouTube search results pages
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
@@ -13,4 +37,3 @@ chrome.runtime.onInstalled.addListener(() => {
     ]);
   });
 });
-
--- content.js
+++ content.js
@@ -1,4 +1,26 @@
-// content.js
+/*
+ * YouTube Mix Blocker Extension
+ *
+ * file: content.js
+ * 
+ * This file is part of the YouTube Mix Blocker Extension.
+ *
+ * Defense YouTube Mix Blocker Extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * YouTube Mix Blocker Extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Defense Against Distractions Extension. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Oleksandr Molodchyk
+ * Copyright (C) 2023-2024 Oleksandr Molodchyk
+ */
 
 // Function to hide the specified element
 function hideElement(element) {
